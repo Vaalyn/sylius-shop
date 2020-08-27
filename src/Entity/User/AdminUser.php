c8ae00d19e622f2d158d5bc08125a347cd2f6cb0
Vaalyn <vaalyndev@gmail.com>
2020-08-27 14:39:09 +0200

    Add odiseoteam/sylius-rbac-plugin, translation, configure access to custom sections of plugins and provide documentation about the feature

--- src/Entity/User/AdminUser.php
+++ src/Entity/User/AdminUser.php
@@ -5,12 +5,15 @@ declare(strict_types=1);
 namespace App\Entity\User;
 
 use Doctrine\ORM\Mapping as ORM;
+use Odiseo\SyliusRbacPlugin\Entity\AdministrationRoleAwareInterface;
+use Odiseo\SyliusRbacPlugin\Entity\AdministrationRoleAwareTrait;
 use Sylius\Component\Core\Model\AdminUser as BaseAdminUser;
 
 /**
  * @ORM\Entity
  * @ORM\Table(name="sylius_admin_user")
  */
-class AdminUser extends BaseAdminUser
+class AdminUser extends BaseAdminUser implements AdministrationRoleAwareInterface
 {
+    use AdministrationRoleAwareTrait;
 }
