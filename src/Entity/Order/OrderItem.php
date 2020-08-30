3dd603a9f01eba4e0885432f0e26e7fa0965036f
Vaalyn <vaalyndev@gmail.com>
2020-08-30 17:56:09 +0200

    Add brille24/sylius-customer-options-plugin with templates and configuration for additional product options

--- src/Entity/Order/OrderItem.php
+++ src/Entity/Order/OrderItem.php
@@ -6,6 +6,8 @@ namespace App\Entity\Order;
 
 use BitBag\SyliusProductBundlePlugin\Entity\ProductBundleOrderItemInterface;
 use BitBag\SyliusProductBundlePlugin\Entity\ProductBundleOrderItemsAwareTrait;
+use Brille24\SyliusCustomerOptionsPlugin\Entity\OrderItemInterface as ProductCustomerOptionOrderItemInterface;
+use Brille24\SyliusCustomerOptionsPlugin\Traits\OrderItemCustomerOptionCapableTrait;
 use Doctrine\ORM\Mapping as ORM;
 use Sylius\Component\Core\Model\OrderItem as BaseOrderItem;
 
@@ -13,9 +15,12 @@ use Sylius\Component\Core\Model\OrderItem as BaseOrderItem;
  * @ORM\Entity
  * @ORM\Table(name="sylius_order_item")
  */
-class OrderItem extends BaseOrderItem
+class OrderItem extends BaseOrderItem implements ProductCustomerOptionOrderItemInterface
 {
     use ProductBundleOrderItemsAwareTrait;
+    use OrderItemCustomerOptionCapableTrait {
+        __construct as protected customerOptionCapableConstructor;
+    }
 
     /**
      * @ORM\OneToMany(targetEntity="BitBag\SyliusProductBundlePlugin\Entity\ProductBundleOrderItemInterface", mappedBy="orderItem", cascade={"all"})
@@ -23,4 +28,11 @@ class OrderItem extends BaseOrderItem
      * @var ArrayCollection|ProductBundleOrderItemInterface[]
      */
     protected $productBundleOrderItems;
+
+    public function __construct()
+    {
+        parent::__construct();
+
+        $this->customerOptionCapableConstructor();
+    }
 }
