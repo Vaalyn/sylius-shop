d450dcfb414f8d0b082774a9d64189e5a77fe399
Vaalyn <vaalyndev@gmail.com>
2020-11-06 01:56:19 +0100

    Add missing mollie plugin trait to order entity

--- src/Entity/Order/Order.php
+++ src/Entity/Order/Order.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace App\Entity\Order;
 
+use BitBag\SyliusMolliePlugin\Entity\OrderInterface;
+use BitBag\SyliusMolliePlugin\Entity\OrderTrait;
 use Doctrine\ORM\Mapping as ORM;
 use Sylius\Component\Core\Model\Order as BaseOrder;
 
@@ -11,6 +13,13 @@ use Sylius\Component\Core\Model\Order as BaseOrder;
  * @ORM\Entity
  * @ORM\Table(name="sylius_order")
  */
-class Order extends BaseOrder
+class Order extends BaseOrder implements OrderInterface
 {
+    use OrderTrait;
+
+    /**
+     * @var bool
+     * @ORM\Column(type="boolean")
+     */
+    protected  $abandonedEmail = false;
 }
